0e95f589d0be5153ef40f6030ff2b28e75391547
tunde <icepaigez@gmail.com>
2022-01-02 09:01:35 +0100

    created a mock pricefeed oracle contract

--- fund_me/contracts/FundMe.sol
+++ fund_me/contracts/FundMe.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.6.6;
+pragma solidity ^0.6.0;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
@@ -7,12 +7,14 @@ import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
 contract FundMe {
 
     using SafeMathChainlink for uint256;
+    AggregatorV3Interface public priceFeed;
 
     address public owner;
     mapping (address => uint256) public donors;
 
-    constructor() public {
+    constructor(address _priceFeed) public {
         owner = msg.sender;
+        priceFeed = AggregatorV3Interface(_priceFeed);
     }
 
     modifier onlyOwner {
@@ -25,7 +27,6 @@ contract FundMe {
     }
 
     function getPrice() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
         (, int price,,,) = priceFeed.latestRoundData();
         return uint256(price);
     }
--- fund_me/migrations/2_deploy_contract.js
+++ fund_me/migrations/2_deploy_contract.js
@@ -1,5 +1,20 @@
 const FundMe = artifacts.require("FundMe");
+const MockV3Aggregator = artifacts.require("MockV3Aggregator");
+
+let priceFeedAddress;
+const decimals = 18
+const initialValue = 2000*10**18
 
 module.exports = async function (deployer, network) {
-  await deployer.deploy(FundMe);
+
+  if (network !== 'development') {
+    priceFeedAddress = 0x9326BFA02ADD2366b30bacB125260Af641031331
+    await deployer.deploy(FundMe, priceFeedAddress);
+  } else {
+    await deployer.deploy(MockV3Aggregator, decimals, initialValue);
+    const mock_aggregator = await MockV3Aggregator.deployed();
+    priceFeedAddress = mock_aggregator.address;
+    console.log(priceFeedAddress)
+    await deployer.deploy(FundMe, priceFeedAddress);
+  }
 };
